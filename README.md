47cf9651fccebb5cd3fdeb5aca22f86df80c5070
maany <imptodefeat@gmail.com>
2022-02-02 12:03:27 +0100

    release version 2

--- README.md
+++ README.md
@@ -1,22 +1,26 @@
 ## Summary
-This script evalues the httpd access logs for Rucio WebUI deployments
-and analysis the frequency of hitting the different endpoints.
+This script processes the httpd access logs for Rucio WebUI deployments
+and generates the frequency of accessing the different endpoints.
 
 ## Usage
 
 *NOTE: requires Python3*
 ```
-python3 rucio_access_log_parser.py [-h] -f HTTPD_ACCESS_LOG_FILE [-o OUTPUT_FILE]
+python3 rucio_access_log_parser.py [-h] -f HTTPD_ACCESS_LOG_FILE [-o OUTPUT_FILE] [-p]
+
+Rucio Access Log Parser
 
 optional arguments:
   -h, --help            show this help message and exit
   -f HTTPD_ACCESS_LOG_FILE
                         The path to httpd access logs to parse
   -o OUTPUT_FILE        The output file
+  -p                    Indicate logs from puppet managed nodes. (Only for Atlas)
+
 ```
 ## Author
 Mayank Sharma <mayank.sharma@cern.ch>
 
 
 ## Version 
-1.0
+2.0
--- rucio_access_log_parser.py
+++ rucio_access_log_parser.py
@@ -1,36 +1,40 @@
 """[summary]
-This script evalues the httpd access logs for Rucio WebUI deployments
-and analysis the frequency of hitting the different endpoints.
+This script processes the httpd access logs for Rucio WebUI deployments
+and generates the frequency of accessing the different endpoints.
 
 [usage]
 
 NOTE: requires Python3
 
-python3 rucio_access_log_parser.py [-h] -f HTTPD_ACCESS_LOG_FILE [-o OUTPUT_FILE]
+python3 rucio_access_log_parser.py [-h] -f HTTPD_ACCESS_LOG_FILE [-o OUTPUT_FILE] [-p]
+
+Rucio Access Log Parser
 
 optional arguments:
   -h, --help            show this help message and exit
   -f HTTPD_ACCESS_LOG_FILE
                         The path to httpd access logs to parse
   -o OUTPUT_FILE        The output file
+  -p                    Indicate logs from puppet managed nodes. (Only for Atlas)
 
 [author]
 Mayank Sharma <mayank.sharma@cern.ch>
 
 
 [version] 
-1.0
+2.0
 """
 
 import argparse
-from functools import partial, reduce
 import logging
+from pydoc import describe
 import re
 import json
 
 logger = logging.getLogger(__name__)
-
-REGEX = r"(?P<ip>.*?) - - \[(?P<timestamp>.*?)\] \"(?P<verb>.*?) (?P<url>.*) .*\" (?P<status_code>\d*) .*"
+VERSION = 2
+REGEX_PUPPET = r"(?P<ip>.*?) - - \[(?P<timestamp>.*?)\] \"(?P<verb>.*?) (?P<url>.*) .*\" (?P<status_code>\d*) .*"
+REGEX_K8S = r"\[(?P<timestamp>.*?)\].*\"(?P<verb>.*?) (?P<url>.*) .*\".*\".*?\""
 COMMON_URLS = (
     ("/account_rse_usage", "account_rse_usage", "Account RSE Usage"),
     ("/account", "account", "Account"),
@@ -62,7 +66,9 @@ COMMON_URLS = (
 )
 
 
-def process_logs(logfile):
+def process_logs(logfile, puppet):
+    regex, url_group_id = (REGEX_PUPPET, 4) if puppet else (REGEX_K8S, 3)
+
     urls = {f"{url}": (0, []) for url, controller, description in COMMON_URLS}
     total_log_entries = 0
     validated_log_entries = 0
@@ -70,17 +76,17 @@ def process_logs(logfile):
     proxied_entries = []
 
     def _match_and_count(match):
-        url = match.group(4)
+        url = match.group(url_group_id)
         for key in urls.keys():
             if url.startswith(key):
                 count, entries = urls[key]
-                entries.append(match.group(4))
+                entries.append(match.group(url_group_id))
                 urls[key] = (count + 1, entries)
                 return True
 
         return False
 
-    validate = lambda log: re.match(REGEX, log)
+    validate = lambda log: re.match(regex, log)
 
     for line in logfile.readlines():
         total_log_entries = total_log_entries + 1
@@ -95,12 +101,13 @@ def process_logs(logfile):
 
     urls = sorted(urls.items(), key=lambda d: d[1][0], reverse=True)
     output = {
+        "version": VERSION,
         "total": total_log_entries,
         "validated": validated_log_entries,
         "matched": matched_log_entries,
         "unmatched": len(proxied_entries),
         "url_analysis": urls,
-        "proxied_entries": proxied_entries,
+        # "proxied_entries": proxied_entries,
     }
     return json.dumps(output, indent=4)
 
@@ -122,11 +129,18 @@ if __name__ == "__main__":
         type=argparse.FileType("w", encoding="UTF-8"),
         help="The output file",
     )
+    parser.add_argument(
+        "-p",
+        dest="puppet",
+        action="store_true",
+        help="Indicate logs from puppet managed nodes. (Only for Atlas)",
+    )
     args = parser.parse_args()
     httpd_logfile = args.httpd_access_log_file
     output_file = args.output_file
+    puppet = args.puppet
     try:
-        output = process_logs(httpd_logfile)
+        output = process_logs(httpd_logfile, puppet)
         output_file.write(output)
     finally:
         httpd_logfile.close()
